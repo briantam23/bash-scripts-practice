f7457f5a98cb5875defcc6bc8f845d245ce623b0
briantam23 <briantam23@gmail.com>
2019-05-11 11:20:24 -0400

    Added more scripts

--- scripts/quotes.sh
+++ scripts/quotes.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#################################################
+
+#####################################################
 #Syntax to define string variable double quotes
 
 str="Shell scripting article"
@@ -13,4 +14,14 @@ echo $str
 #using variable in a string
 user="ABC"
 str="Shell scripting \"article\" by $user"
+echo $str
+
+
+#####################################################
+#Syntax to define string variable using single quotes
+str='Shell scripting article'
+echo $str
+
+#Trying \ to escape characters in single quotes
+str='Shell scripting \"article\"'
 echo $str

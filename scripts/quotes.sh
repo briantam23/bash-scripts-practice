55c3f4ead383fed386c3cd8d919277ae5b3f9a12
briantam23 <briantam23@gmail.com>
2019-05-11 11:24:26 -0400

    Added more scripts

--- scripts/quotes.sh
+++ scripts/quotes.sh
@@ -25,3 +25,17 @@ echo $str
 #Trying \ to escape characters in single quotes
 str='Shell scripting \"article\"'
 echo $str
+
+
+#####################################################
+#Syntax to define a string variable using left quotes
+
+#Anything enclosed in left quotes will be treated as an executable command
+
+print_date() {
+    echo "Today is `date +"%A %d %B %Y (%r)"`"
+    return
+}
+
+str="Current date is `print_date`"
+echo $str
